c46d5149ea5bbe0e55aa7f064572a407215b690d
Lebranc Convas <lebrancconvas@gmail.com>
2017-12-23 17:38:12 +0700

    Add nested loop
    Add nested loop about Multiplication Table from 1 to 12.

--- looptest.py
+++ looptest.py
@@ -1,5 +1,12 @@
-def a():
+def for_loop():
     month = "January" , "February" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"
     for m in range(len(month)):
         print(m + 1,"->",month[m])
-a()
+        
+def nested_loop():
+    for a in range(1,13,1):
+        print("------------")
+        for b in range(1,13,1):
+            print(a,"x",b,"=",a*b)
+for_loop()
+nested_loop()
