9b125acac6c30483639b210b28060e773cc52e52
Lebranc Convas <lebrancconvas@gmail.com>
2017-12-23 17:30:14 +0700

    For Loop Code
    For loop code testing about the topic "month" .

---
+++ looptest.py
@@ -0,0 +1,5 @@
+def a():
+    month = "January" , "February" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"
+    for m in range(len(month)):
+        print(m + 1,"->",month[m])
+a()
