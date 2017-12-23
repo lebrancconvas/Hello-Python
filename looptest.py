23c47563b679a16be97555a8e503ef2cd0973aa3
Lebranc Convas <lebrancconvas@gmail.com>
2017-12-23 19:20:01 +0700

    Add money function while loop

--- looptest.py
+++ looptest.py
@@ -19,8 +19,26 @@ def while_loop():
             print("You pass!!, Welcome to my website.")
             break
             
-
+def loop_test():
+    while True:
+        m = int(input("Insert your money : "))
+        if (m < 500):
+            while True:
+                name = input("Input your name (Max 20 Char) : ")
+                if (len(name) <= 20):
+                    print("Ok , " , name , "I think you should try to spend less.")
+                    break
+                else:
+                    print("--ERROR , Insert your name again--")
+                    continue
+            break
+        else:
+            print("Ok! Good job. Now you have ",m," Bath")
+            break
+                
+    
     
 for_loop()
 nested_loop()
 while_loop()
+loop_test()
