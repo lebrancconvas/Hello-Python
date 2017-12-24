e12b784b60cd030849b6eaab0048e0ea448599df
Lebranc Convas <lebrancconvas@gmail.com>
2017-12-24 08:43:01 +0700

    Create a lucky number game
    predict the lucky number from random method.

--- randtest.py
+++ randtest.py
@@ -1,17 +1,25 @@
-import random
-inp = int(input("Insert your predict value: "))
-if(inp >= 0 and inp < 100):
-    for i in range(9):
-        value = random.randint(1,100)
-        if (value == inp):
-            print(value,"-> That's a right number")
-        elif(value > inp):
-            print(value,"-> Too much!!")
-        elif(value < inp):
-            print(value,"-> Too less")
+import random 
+value = random.randint(1,99)
+while True:
+    try:
+        inp = int(input("Insert your predict value(Only 0-99): "))
+        if(inp >= 0 and inp < 100):
+            for i in range(1):
+                if (value == inp):
+                    print("-> That's a right number")
+                    print("Good Job , Thank you for participation.")
+                elif(value < inp):
+                    print("-> Too much!!")
+                elif(value > inp):
+                    print("-> Too less")
+                else:
+                    print("--ERROR--")
         else:
             print("--ERROR--")
-else:
-    print("--ERROR--")
+    except:
+        print("--End The Program--")
+        break
+        
+        
             
    
