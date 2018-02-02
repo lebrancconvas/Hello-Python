0fd6dc83da8d5e927e99a0fa0c3999bfc357c923
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-03 04:32:52 +0700

    Add The Second Way to change float number to round number.

--- float_round.py
+++ float_round.py
@@ -6,7 +6,16 @@ def floatround():
     int_deci = int(str_deci)
     first_index = (str(x))[:str(x).find(".")]
     if(int_deci < 5):
-    print(int(first_index))
+        print(int(first_index))
     else:
-    print(int(first_index) + 1)
+        print(int(first_index) + 1)
+
+def floatround2():
+    x = float(input("Input your second float number : "))
+    num = x + 0.5
+    s = str(num)
+    point = s.find(".")
+    print(s[:point])
+
 floatround()
+floatround2()
