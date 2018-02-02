fc3c5c57c3affc0a326793ffd03df9893882bae8
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-03 04:25:29 +0700

    Capsulate Code with floatround() Function.

--- float_round.py
+++ float_round.py
@@ -1,10 +1,12 @@
 # Change float number to round number
-x = float(input("Input your float number : "))
-index_after = str(x).find(".")
-str_deci = (str(x))[index_after+1]
-int_deci = int(str_deci)
-first_index = (str(x))[:str(x).find(".")]
-if(int_deci < 5):
-  print(int(first_index))
-else:
-  print(int(first_index) + 1)
+def floatround():
+    x = float(input("Input your float number : "))
+    index_after = str(x).find(".")
+    str_deci = (str(x))[index_after+1]
+    int_deci = int(str_deci)
+    first_index = (str(x))[:str(x).find(".")]
+    if(int_deci < 5):
+    print(int(first_index))
+    else:
+    print(int(first_index) + 1)
+floatround()
