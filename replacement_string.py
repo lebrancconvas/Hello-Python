a81e665c96e9585aae59313b19c6dd063fecbef7
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-04 06:40:02 +0700

    Encapsule with Function and Edit Something.

--- replacement_string.py
+++ replacement_string.py
@@ -1,19 +1,24 @@
-print("WARNNING!! marker should be the word that occur in line.")
-marker = input("Input your word : ")
-replacement = input("Input your replacement word : ")
-line = input("Input your line sentence : ")
+def replaced_string():
+    print("WARNNING!! your word should be the word that occur in your line sentence.")
+    line = input("Input your line sentence : ")
+    marker = input("Input your word : ")
+    replacement = input("Input your replacement word : ")
+    
+    
+    find_mark = line.find(marker)
+    before_line_mark = line[:find_mark]
+    before_replaced = before_line_mark + replacement
+    after_line_mark_with_marker = line[find_mark:]
+    after_line_mark = after_line_mark_with_marker[len(marker):]
+    replaced = before_replaced + after_line_mark
 
 
-find_mark = line.find(marker)
-before_line_mark = line[:find_mark]
-before_replaced = before_line_mark + replacement
-after_line_mark_with_marker = line[find_mark:]
-after_line_mark = after_line_mark_with_marker[len(marker):]
-replaced = before_replaced + after_line_mark
+    #print(before_line_mark)
+    #print(after_line_mark_with_marker)
+    #print(after_line_mark)
+    #print(before_replaced)
+    print("")
+    print("OUTPUT SHOW!!!")
+    print(">> ",replaced)
 
-
-#print(before_line_mark)
-#print(after_line_mark_with_marker)
-#print(after_line_mark)
-#print(before_replaced)
-print(replaced)
+replaced_string()
