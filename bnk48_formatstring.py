c6c44e48ff8a8175df069c993269dc321bc5a348
lebrancconvas <lebrancconvas@gmail.com>
2018-06-04 22:40:39 +0700

    Format String Test.

---
+++ bnk48_formatstring.py
@@ -0,0 +1,8 @@
+#ชราไลน์ : แก้ว, แจน, นํ้าหนึ่ง, เนย, ตาหวาน และอร
+#กุมารไลน์ : ซัทจัง, จ๋า, เปี่ยม, จิ๊บ, โมบาย, คิทแคท, มายด์, เคท และนํ้าใส  
+
+bnk_Charaline = ["Kaew","Jan","Namnueng","Noey","Tarwann","Orn"]
+bnk_Gumanline = ["Satchan","Jaa","Piam","Jib","Mobile","Kitkat","Mild","Kate","Namsai"]
+msg = "BNK48 Charaline Member : {0} {1} {2} {3} {4} {5}".format(bnk_Charaline[0],bnk_Charaline[1],bnk_Charaline[2],bnk_Charaline[3],bnk_Charaline[4],bnk_Charaline[5])
+print(msg)
+
